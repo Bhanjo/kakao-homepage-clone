8d77dc6121ae441e794b383d10c818486a521835
Bhanjo <hanjo1515@naver.com>
2020-11-03 17:29:01 +0900

    20_11_03

--- menubar.js
+++ menubar.js
@@ -1,22 +1,32 @@
+// 드롭다운 메뉴바 코드
+
 const kakaoMenu = document.getElementById("hoverKakao");
 const kakaoDetail = document.getElementById("kakaoDetail");
 kakaoMenu.addEventListener("mouseover", showKakao);
 kakaoMenu.addEventListener("mouseout", hideKakao);
+kakaoDetail.addEventListener("mouseover", showKakao);
+kakaoDetail.addEventListener("mouseout", hideKakao);
 
 const investMenu = document.getElementById("hoverInvest");
 const investDetail = document.getElementById("investmentDetail")
 investMenu.addEventListener("mouseover", showInvest);
 investMenu.addEventListener("mouseout", hideInvest);
+investDetail.addEventListener("mouseover", showInvest);
+investDetail.addEventListener("mouseout", hideInvest);
 
 const serviceMenu = document.getElementById("hoverService");
 const serviceDetail = document.getElementById("serviceDetail");
 serviceMenu.addEventListener("mouseover", showService);
 serviceMenu.addEventListener("mouseout", hideService);
+serviceDetail.addEventListener("mouseover", showService);
+serviceDetail.addEventListener("mouseout", hideService);
 
 const socialMenu = document.getElementById("hoverSocial");
 const socialDetail = document.getElementById("socialDetail");
 socialMenu.addEventListener("mouseover", showSocial);
 socialMenu.addEventListener("mouseout", hideSocial);
+socialDetail.addEventListener("mouseover", showSocial);
+socialDetail.addEventListener("mouseout", hideSocial);
 
 // 카카오 메뉴
 function showKakao() {
