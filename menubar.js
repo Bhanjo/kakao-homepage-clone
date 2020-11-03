5ac434863a99ec82cf93584f26eac7d997ab28a7
Bhanjo <hanjo1515@naver.com>
2020-11-03 13:49:47 +0900

    20_11_03

---
+++ menubar.js
@@ -0,0 +1,51 @@
+const kakaoMenu = document.getElementById("hoverKakao");
+const kakaoDetail = document.getElementById("kakaoDetail");
+kakaoMenu.addEventListener("mouseover", showKakao);
+kakaoMenu.addEventListener("mouseout", hideKakao);
+
+const investMenu = document.getElementById("hoverInvest");
+const investDetail = document.getElementById("investmentDetail")
+investMenu.addEventListener("mouseover", showInvest);
+investMenu.addEventListener("mouseout", hideInvest);
+
+const serviceMenu = document.getElementById("hoverService");
+const serviceDetail = document.getElementById("serviceDetail");
+serviceMenu.addEventListener("mouseover", showService);
+serviceMenu.addEventListener("mouseout", hideService);
+
+const socialMenu = document.getElementById("hoverSocial");
+const socialDetail = document.getElementById("socialDetail");
+socialMenu.addEventListener("mouseover", showSocial);
+socialMenu.addEventListener("mouseout", hideSocial);
+
+// 카카오 메뉴
+function showKakao() {
+    kakaoDetail.style.display = 'flex';
+}
+function hideKakao() {
+    kakaoDetail.style.display = 'none';
+}
+
+//투자 메뉴
+function showInvest() {
+    investDetail.style.display = 'flex';
+}
+function hideInvest() {
+    investDetail.style.display = 'none';
+}
+
+//서비스 메뉴
+function showService() {
+    serviceDetail.style.display = 'flex';
+}
+function hideService() {
+    serviceDetail.style.display = 'none';
+}
+
+//소셜임팩트 메뉴
+function showSocial() {
+    socialDetail.style.display = 'flex';
+}
+function hideSocial() {
+    socialDetail.style.display = 'none';
+}
